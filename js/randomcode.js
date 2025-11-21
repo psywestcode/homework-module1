ebe084753f5129e7e7c4b2295a78ca28e9eb78c7
Sailesh Mohapatra <s.mohapatra5530@westcliff.edu>
2025-11-20 18:36:46 -0800

    fixed color change js issue

--- js/randomcode.js
+++ js/randomcode.js
@@ -1,20 +1,20 @@
 /* ------ RANDOM CODES ------ */
-var code = ' '; //to store generated codes and initialize to empty value
-var getCode = ' '; //to store entered code
-var btnvalue; //for button boolean value
+
+// NOTE: submit button is initially disabled upon loading of this page - see <body> in html
+
+var code = ' '; // to store generated codes and initialize to empty value
+var getCode = ' '; // to store entered code
+var btnvalue; // for button boolean value
+// create variable to hold the type of characters we want to show as codes
 var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 
 // Function to generate combination of characters
 function generateCode() {
-    // Create variables to store generated codes and the type of characters we want to show as codes
-    
-
     // Generate character multiple times using a loop
     for (i = 1; i <= 8; i++) {
-        var char = Math.random() * str.length; // random select a character from the variable and then store in a new variable
-        code += str.charAt(char) // accumulate the generated character into a string of 8 characters
+        var char = Math.random() * str.length; // random select a character from the variable
+        code += str.charAt(char); // accumulate the generated character into a string of 8 characters
     }
-    
     return code; // return the final accumulated string when loop ends
 }
 
@@ -23,20 +23,17 @@ document.getElementById("codes").innerHTML = generateCode();
 
 // Disable Button
 function disableButton(btnvalue) {
-    document.getElementById("submit").disabled = btnvalue;
-    if (btnvalue == true) {
-        //set button and label color translucent
-        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
-        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    document.getElementById("Submit").disabled = btnvalue; // enable/disable button
+    if (btnvalue == true) { // test if button is disabled or enabled
+        // set button and label color translucent
+        document.getElementById("Submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("Submit").style.color = "rgba(255, 255, 255, 0.5)";
     } else {
-        //set button and label color with no transparency
-        document.getElementById("submit").style.backgroundColor = "rgba(234, 41, 11, 1)";
-        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+        // set button and label color with no transparency
+        document.getElementById("Submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("Submit").style.color = "rgba(255, 255, 255, 1)";
     }
 }
-
-// // Activate function
-// disableButton();
 
 // Listen to user input code
 var codebox = document.getElementById("codeentered"); // get textbox
