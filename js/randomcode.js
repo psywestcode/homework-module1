ddc672c564c7a391d3a1d471cf9b336319274cd7
Sailesh Mohapatra <s.mohapatra5530@westcliff.edu>
2025-11-20 18:13:12 -0800

    global var w4d2h commit

--- js/randomcode.js
+++ js/randomcode.js
@@ -1,10 +1,13 @@
 /* ------ RANDOM CODES ------ */
+var code = ' '; //to store generated codes and initialize to empty value
+var getCode = ' '; //to store entered code
+var btnvalue; //for button boolean value
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 
 // Function to generate combination of characters
 function generateCode() {
     // Create variables to store generated codes and the type of characters we want to show as codes
-    var code = ""; // initialize to null value
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+    
 
     // Generate character multiple times using a loop
     for (i = 1; i <= 8; i++) {
