4253c6b97ea529b9a392906d1e00a0a89b708a2c
Sailesh Mohapatra <s.mohapatra5530@westcliff.edu>
2025-11-20 18:19:03 -0800

    update disable button

--- js/randomcode.js
+++ js/randomcode.js
@@ -22,9 +22,34 @@ function generateCode() {
 document.getElementById("codes").innerHTML = generateCode();
 
 // Disable Button
-function disableButton() {
-    document.getElementById("Submit").disabled = true;
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true) {
+        //set button and label color translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        //set button and label color with no transparency
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-// Activate function
-disableButton();
+// // Activate function
+// disableButton();
+
+// Listen to user input code
+var codebox = document.getElementById("codeentered"); // get textbox
+codebox.addEventListener("input", evaluateCode); // listen to code entered in textbox
+
+// Run function if detected user had entered a character in textbox
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value; // get character entered
+    var charset1 = getCode.trim(); // remove any hidden characters entered
+    var charset2 = code.trim(); // remove any hidden characters generated
+    
+    // test if code entered matches the number of generated characters
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false); // if match, run the function to enable button
+    }
+}
