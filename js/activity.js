2542e50af50a70422402664119dac2020a45e719
Sailesh Mohapatra <s.mohapatra5530@westcliff.edu>
2025-12-14 21:35:00 -0800

    activities modal

--- js/activity.js
+++ js/activity.js
@@ -6,50 +6,35 @@ $(document).ready(function() {
         var content = $(this).text(); // Get content of the clicked cell
 
         // Check if content matches "Not Available"
-        // If it DOES NOT contain "Not Available", proceed
         if (content !== "Not Available") {
             
-            // Toggle the highlight class
+            // Toggle the highlight class on the clicked cell
             $(this).toggleClass("tdhighlight");
 
-            // --- Advanced Feature: Extract Cliff Name ---
-            // 1. Get the index of the clicked cell within its row
-            // (Note: In the tbody, the first column is a 'th' (Hiking, etc), 
-            // so index 1 corresponds to 'West Cliff' which is also index 1 in thead)
-            var colIndex = $(this).index();
+            // --- Logic to Update Modal Content ---
             
-            // 2. Use that index to find the text in the table header
-            var cliffName = $("table thead th").eq(colIndex).text();
+            // 1. Clear current modal content
+            var modalBody = $('#activityModal .modal-body');
+            modalBody.empty();
             
-            // 3. Combine activity + cliff name
-            var fullText = content + " at " + cliffName;
-            // ---------------------------------------------
-
-            if ($(this).hasClass("tdhighlight")) {
+            // 2. Loop through ALL highlighted cells to rebuild the list
+            if ($("tbody td.tdhighlight").length > 0) {
                 
-                // Item Selected:
-                // Make display box visible
-                $('#displaySelected').css("visibility", "visible");
-                $('#displaySelected').css("margin-top", "2em");
-                
-                // Add the activity to the result box
-                // We use the fullText (Activity + Cliff) here
-                $('#result').append("<p>" + fullText + "</p>");
+                $("tbody td.tdhighlight").each(function() {
+                    var cellText = $(this).text();
+                    var colIndex = $(this).index();
+                    var cliffName = $("table thead th").eq(colIndex).text();
+                    
+                    // Append text to modal body
+                    modalBody.append("<p>" + cellText + " at " + cliffName + "</p>");
+                });
 
+                // 3. Show the Modal
+                $('#activityModal').modal('show');
+                
             } else {
-                
-                // Item Deselected:
-                // Remove the paragraph that contains the text
-                // Note: We search for 'content' to ensure we grab the right one, 
-                // but checking fullText is safer if descriptions are unique. 
-                // Given the homework constraints, containing content is usually sufficient.
-                $('#result p:contains(' + content + ')').remove();
-
-                // Check if there are any paragraphs left. If false, hide the box.
-                if ($('#result').has('p').length === false) {
-                    $('#displaySelected').css("visibility", "hidden");
-                    $('#displaySelected').css("margin-top", "0");
-                }
+                // If nothing is selected, you might ensure modal is closed, 
+                // but usually the user closes it manually.
             }
         }
     });
